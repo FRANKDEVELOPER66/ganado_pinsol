d7dc3a0de11e6f193d721b851939b7b09ff49b0d
danielgarcia11366 <danielgarcia11366@gmail.com>
2026-05-10 17:43:59 -0600

    listo

--- views/pages/index.php
+++ views/pages/index.php
@@ -4,8 +4,8 @@
   </div>
 </div>
 <div class="row justify-content-center">
-  <div class="col-lg-4">
-    <img src="./images/cit.png" width="100%" alt="">
+  <div class="col-lg-8">
+    <img src="./images/pinsol.png" width="100%" alt="">
   </div>
 </div>
 <script src="build/js/inicio.js"></script>
